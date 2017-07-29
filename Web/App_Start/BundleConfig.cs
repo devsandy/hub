17c343d7ef8c1c2657f59fe52147fb064eb682a7
Sandeep <Sandeep@SANDEEP>
2017-07-29 17:38:34 +0530

    Add project files.

---
+++ Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+namespace Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles ) {
+
+            #region Scripts
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-{version}.js"));
+            #endregion
+
+            #region Styles
+            bundles.Add(new LessBundle("~/Content/css").Include("~/Content/bootstrap/bootstrap.less"));
+            #endregion
+        }
+    }
+}
